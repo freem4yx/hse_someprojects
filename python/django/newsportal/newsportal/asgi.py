96e6205f9e0dcd92e8c5e9aa66b7c95a654559f5
alexandra2704 <58882562+alexandra2704@users.noreply.github.com>
2025-12-23 09:09:07 +0300

    newsportal

--- python/django/newsportal/newsportal/asgi.py
+++ python/django/newsportal/newsportal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for newsport project.
+ASGI config for newsportal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsport.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsportal.settings')
 
 application = get_asgi_application()
